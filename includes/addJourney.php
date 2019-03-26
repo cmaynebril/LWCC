81e0cdaaadb525f452dc7dd5231b310d791cda9c
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-03-26 14:07:18 +0800

    change P to Done

--- includes/addJourney.php
+++ includes/addJourney.php
@@ -14,13 +14,13 @@ if (isset($_POST['SaveJourney']))
                                         if($queryResult > 0)
                                         {
                                             //query for update
-                                            $sql1 = "UPDATE tbllifeprocess SET Name='$name',NetworkLeader='$networkLeader',Mentor='$mentor', ".$process."='P', dt".$process."='$dateProcessed'
+                                            $sql1 = "UPDATE tbllifeprocess SET Name='$name',NetworkLeader='$networkLeader',Mentor='$mentor', ".$process."='Done', dt".$process."='$dateProcessed'
                                             WHERE Name like '%".$name."%'";
                                         }  
                                         else{
                                             //query for insert
                                             $sql1 = "INSERT INTO tbllifeprocess(Name,NetworkLeader,Mentor,".$process.",dt".$process.") 
-                                            VALUES('$name', '$networkLeader', '$mentor', 'P', '$dateProcessed')";
+                                            VALUES('$name', '$networkLeader', '$mentor', 'Done', '$dateProcessed')";
                                         } 
                                         $query = mysqli_query($conn, $sql1) or die(mysqli_error($conn));
                                         header("Location: ../Pages/JourneyAttendance.php?result=saved");
